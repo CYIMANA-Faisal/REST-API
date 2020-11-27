5b9ea7da85f93acf8ed303c334f05c162e65804a
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2020-11-27 07:59:30 +0200

    token handling

--- routes/message.js
+++ routes/message.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/authMiddleware')
 const {
     createMessage,
     getMessages,
     getMessage,
     deleteMessage
 } = require('../controler/message');
+const auth = require('../middlewares/authMiddleware');
 
 router.get('/', getMessages);
-router.get('/:id', getMessage)
+router.get('/:id',auth, getMessage)
 router.post('/', createMessage);
-router.delete('/:id', deleteMessage);
+router.delete('/:id',auth, deleteMessage);
 
 module.exports = router;
